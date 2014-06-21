ebd15eaad80d157fb3c66ce7e5d54d93c123a562
Marcel de Castilho <marcel.castilho@gmail.com>
2014-06-21 00:12:52 -0300

    fix menu (onClick)

--- Bolao/Scripts/components/menu-item.jsx
+++ Bolao/Scripts/components/menu-item.jsx
@@ -5,7 +5,7 @@ define(['react'], function (React) {
     render: function () {
       var className = this.props.key === this.props.active ? 'blog-nav-item active' : 'blog-nav-item'; 
       return ( 
-              <a href={BASE_URL + '#/' + this.props.key} className={className} onClick={this.props.handleClick}>{this.props.children}</a>
+              <a href={BASE_URL + '#/' + this.props.key} className={className}>{this.props.children}</a>
              );
     }
   });
--- Bolao/Scripts/components/menu.jsx
+++ Bolao/Scripts/components/menu.jsx
@@ -5,17 +5,14 @@ define(['react', 'jsx!components/menu-item'], function (React, MenuItem) {
     getInitialState: function() {
       return { };
     },
-    handleClick: function(item) {
-      this.setState( { active: item.props.key } );
-    },
     render: function () {
       var active = this.state.active || this.props.currentPage;
       return (
         <nav className="blog-nav">
-          <MenuItem key='ranking' active={active} handleClick={this.handleClick}>Ranking</MenuItem>
-          <MenuItem key='resultados' active={active} handleClick={this.handleClick}>Resultados</MenuItem>
-          <MenuItem key='regras' active={active} handleClick={this.handleClick}>Regras</MenuItem>
-          <MenuItem key='premio' active={active} handleClick={this.handleClick}>Prêmio</MenuItem>
+          <MenuItem key='ranking' active={active}>Ranking</MenuItem>
+          <MenuItem key='resultados' active={active}>Resultados</MenuItem>
+          <MenuItem key='regras' active={active}>Regras</MenuItem>
+          <MenuItem key='premio' active={active}>Prêmio</MenuItem>
         </nav>
       );
     }
