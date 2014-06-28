9fd0af053daa07c08ef366ca6ea07d8c48667823
Marcel de Castilho <marcel.castilho@gmail.com>
2014-06-27 22:40:15 -0300

    considera peso na pontuação por fase

--- Bolao/Scripts/app/aposta.js
+++ Bolao/Scripts/app/aposta.js
@@ -7,13 +7,24 @@ define(function () {
     this.pontos = 0;
   };
 
+  var getPeso = function (jogo) {
+    if (jogo <= 48) {
+      return 1;
+    } else if (jogo <= 56) {
+      return 2;
+    } else if (jogo <= 60) {
+      return 3;
+    }
+    return 5;
+  };
+
   Aposta.prototype.totalize = function (results) {
     this.pontos = 0;
     for (var i = 0; i < this.placares.length; i++) {
       if (this.placares[i].acertou(results[i])) {
-        this.pontos += 3;
+        this.pontos += 3 * getPeso(i + 1);
       } else if (this.placares[i].acertouVencedor(results[i])) {
-        this.pontos += 1;
+        this.pontos += 1 * getPeso(i + 1);
       }
     }
   };
